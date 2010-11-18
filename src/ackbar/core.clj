067ccfbc97183fa095fae3429479a1745f3a5fc8
Derek Thurn <derek@thurn.ca>
2010-11-17 21:52:16 -0800

    Added the ability to create new pages.

--- src/ackbar/core.clj
+++ src/ackbar/core.clj
@@ -1,6 +1,17 @@
 (ns ackbar.core
   (:use compojure.core hiccup.core hiccup.form-helpers hiccup.page-helpers)
-  (:require [appengine-magic.core :as ae]))
+  (:require [appengine-magic.core :as ae]
+            [appengine-magic.services.datastore :as ds]
+            [clojure.contrib.str-utils2 :as s])
+  (:import com.google.appengine.api.datastore.EntityNotFoundException))
+
+(ds/defentity Page [^:key name, body])
+
+(defn canonical-name
+  "Converts a given page name to a standard format, suitable for use in a URL."
+  [str]
+  (s/replace (s/replace (.toLowerCase str) #"\s+" "_") #"\W+" ""))
+
 
 (defn wrap-result [result]
   {:status 200
@@ -10,22 +21,27 @@
 
 (defn result [str] (wrap-result {:title str :body [:h1 str]}))
 
-(defn response-wrapper [title body]
-  {:status 200
-   :headers {"Content-Type" "text/html"}
-   :body (xhtml [:head [:title title]]
-                [:body body])})
+(defn response-wrapper
+  ([title body] (response-wrapper title body 200))
+  ([title body status]
+   {:status status
+    :headers {"Content-Type" "text/html"}
+    :body (xhtml [:head [:title title]]
+                 [:body body])}))
 
 (defn render-page
   "Retrieve a page from the datastore and render it for display to the user"
-  [page]
-  (result (str "Rendered Page: " page)))
+  [name]
+  (try
+    (response-wrapper name (:body (ds/retrieve Page name)))
+    (catch EntityNotFoundException _
+        (response-wrapper "404" (str "Unable to find page: " name) 404))))
 
 (defn admin-add-page
   "Displays the Ackbar Admin 'Add Page' Page"
   []
   (response-wrapper "Add Page"
-   (form-to [:post "/admin/add-page"]
+   (form-to [:post "/admin/add_page"]
             (label "title" "Title:")
             [:br]
             (text-field "title")
@@ -36,6 +52,13 @@
             [:br]
             (submit-button "Submit")
             )))
+
+(defn add-page
+  "Adds a page to the database with the specified title and content"
+  [title body]
+  (ds/save! (Page. (canonical-name title) body))
+  (result "Page saved!")
+  )
 
 (defn admin-edit-page
   "Displays the Ackbar Admin 'Edit Page' Page"
@@ -57,23 +80,18 @@
   []
   (result "Combined CSS"))
 
-(defn canonical-name
-  "Converts a given page name to a standard format, suitable for use in a URL."
-  [str]
-  str)
-
-                                        ; Special Pages: home, admin, 404, navbar
 (defroutes ackbar-app-handler
   (GET "/" [] (render-page "home"))
-  (GET "/admin/add-page" [] (admin-add-page))
-  (POST "/admin/add-page" {params :params} (render-page "add page post"))
-  (GET "/admin/edit-page" [] (admin-edit-page))
-  (POST "/admin/edit-page" {params :params} (render-page "edit page post"))
-  (GET "/admin/delete-page" [] (admin-delete-page))
-  (POST "/admin/delete-page" {params :params} (render-page "delete page post"))
+  (GET "/admin/add_page" [] (admin-add-page))
+  (POST "/admin/add_page" {params :params}
+        (add-page (params "title") (params "body")))
+  (GET "/admin/edit_page" [] (admin-edit-page))
+  (POST "/admin/edit_page" {params :params} (render-page "edit page post"))
+  (GET "/admin/delete_page" [] (admin-delete-page))
+  (POST "/admin/delete_page" {params :params} (render-page "delete page post"))
   (GET "/static/combined.js" [] (combined-js))
   (GET "/static/combined.css" []  (combined-css))
   (GET "/:name" [name] (render-page (canonical-name name)))
-  (ANY "*" [] (render-page "404")))
+  (ANY "*" [] (response-wrapper "404" "NOT FOUND!" 404)))
 
 (ae/def-appengine-app ackbar-app #'ackbar-app-handler)
