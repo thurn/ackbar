d5a6c45af2fe5baf57b1733b22b698732406120c
Derek Thurn <derek@thurn.ca>
2010-11-20 18:06:02 -0800

    Added pagination for the home page.

--- src/ackbar/core.clj
+++ src/ackbar/core.clj
@@ -5,7 +5,7 @@
             [clojure.contrib.str-utils2 :as s])
   (:import com.google.appengine.api.datastore.EntityNotFoundException))
 
-(ds/defentity Page [^:key title, body])
+(ds/defentity Page [^:key url, title, body, timestamp])
 
 (defn canonical-title
   "Converts a given page title to a standard format, suitable for use in a URL."
@@ -30,11 +30,11 @@
 
 (defn render-page
   "Retrieve a page from the datastore and render it for display to the user"
-  [title]
+  [url]
   (try
-    (response-wrapper title (:body (ds/retrieve Page title)))
+    (response-wrapper url (:body (ds/retrieve Page url)))
     (catch EntityNotFoundException _
-      (response-wrapper "404" (str "Unable to find page: " title) 404))))
+      (response-wrapper "404" (str "Unable to find page: " url) 404))))
 
 (defn add-or-edit-form
   "Returns a form to add or edit a page."
@@ -46,7 +46,7 @@
               (label "body" "Body:") [:br]
               (text-area {:cols 80 :rows 40} "body" body) [:br]
               (submit-button "Submit"))))
-                                 
+
 (defn admin-add-page
   "Displays the Ackbar Admin 'Add Page' Page"
   []
@@ -55,26 +55,26 @@
 
 (defn add-page
   "Adds a page to the database with the specified title and content"
-  [title body]
-  (if (ds/exists? Page title)
+  [url title body]
+  (if (ds/exists? Page url)
     (result "ERROR: Page already exists!")
-    (do (ds/save! (Page. (canonical-title title) body))
+    (do (ds/save! (Page. url title body (System/currentTimeMillis)))
         (result "Page saved!"))))
 
 (defn page-link
   "Builds a link to the specified page object"
   [page]
-  (link-to (str "/" (:title page)) (:title page)))
+  (link-to (str "/" (:url page)) (:title page)))
 
 (defn edit-link
   "Builds a link to edit the specified page"
   [page]
-  (link-to (str "/admin/edit/" (:title page)) "edit"))
+  (link-to (str "/admin/edit/" (:url page)) "edit"))
 
 (defn delete-link
   "Builds a link to delete the specified page"
   [page]
-  (form-to [:post (str "/admin/delete/" (:title page))]
+  (form-to [:post (str "/admin/delete/" (:url page))]
            (submit-button "Delete")))
 
 (defn admin-page-list
@@ -89,34 +89,34 @@
                               (edit-link page) (delete-link page)])])))
 
 (defn delete-page
-  "Deletes the specified page by title."
-  [title]
+  "Deletes the specified page by url"
+  [url]
   (try
-    (do (ds/delete! (ds/retrieve Page title))
+    (do (ds/delete! (ds/retrieve Page url))
         (response-wrapper "Deleted"
-                          (str "Page deleted: " title)))
-      
+                          (str "Page deleted: " url)))
+    
     (catch EntityNotFoundException _
-      (response-wrapper "404" (str "Unable to find page: " title) 404))))
+      (response-wrapper "404" (str "Unable to find page: " url) 404))))
 
 (defn admin-edit-page
   "Displays the Ackbar Admin 'Edit Page' Page"
-  [title]
+  [url]
   (try
-    (def page (ds/retrieve Page title))
+    (def page (ds/retrieve Page url))
     (response-wrapper "Edit Page"
                       (add-or-edit-form "/admin/edit"
                                         (:title page)
                                         (:body page)))
     (catch EntityNotFoundException _
-      (response-wrapper "404" (str "Unable to find page: " title) 404))))
+      (response-wrapper "404" (str "Unable to find page: " url) 404))))
 
 (defn edit-page
   "Edits a page, updating its title and content"
-  [title body]
-  (if (ds/exists? Page (canonical-title title))
-    (ds/delete! (ds/retrieve Page (canonical-title title))))
-  (ds/save! (Page. (canonical-title title) body))
+  [url title body]
+  (if (ds/exists? Page url)
+    (ds/delete! (ds/retrieve Page url)))
+  (ds/save! (Page. url title body (System/currentTimeMillis)))
   (result "Page updated!"))
 
 (defn all-pages
@@ -128,19 +128,41 @@
    "All Pages"
    (for [page pages]
      [:div [:h1 (:title page)]
-     [:div (:body page)]])))
-                    
+      [:div (:body page)]])))
+
+(defn render-paginate
+  "Renders a fixed number of pages"
+  [number]
+  (def pagesize 10)
+  (def pages (ds/query :kind Page :limit pagesize
+                       :offset (* pagesize number)
+                       :sort [[:timestamp :dsc]]))
+  (response-wrapper
+   "Paginated"
+   (html (for [page pages]
+           [:div [:h1 (:title page)]
+            [:div (:body page)]])
+         (if (> number 0)
+           (html (link-to (str "/pages/" (dec number)) "prev") [:br]))
+         (if (= pagesize (count pages))
+           (link-to (str "/pages/" (inc number)) "next")))))
+
 (defroutes ackbar-app-handler
-  (GET "/" [] (render-page "home"))
+  (GET "/" [] (render-paginate 0))
+  (GET "/pages/:number" [number] (render-paginate (Integer/parseInt number)))
   (GET "/admin/add" [] (admin-add-page))
   (GET "/admin/pages" [] (admin-page-list))
   (POST "/admin/add" {params :params}
-        (add-page (params "title") (params "body")))
+        (add-page (canonical-title (params "title"))
+                  (params "title")
+                  (params "body")))
   (GET "/admin/edit/:title" [title] (admin-edit-page title))
   (POST "/admin/edit" {params :params}
-        (edit-page (params "title") (params "body")))
+        (edit-page (canonical-title (params "title"))
+                  (params "title")
+                  (params "body")))
   (POST "/admin/delete/:title" [title] (delete-page title))
-  (GET "/all" [] (all-pages))
+  (GET "/pages" [] (all-pages))
   (GET "/:title" [title] (render-page (canonical-title title)))
   (ANY "*" [] (response-wrapper "404" "NOT FOUND!" 404)))
 
