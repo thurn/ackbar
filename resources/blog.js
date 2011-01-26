33d8b64fcfe961627b0857e4bcccd69b0db52252
Derek Thurn <derek@thurn.ca>
2011-01-26 12:50:55 -0500

    Implemented basic support for JavaScript revealed "discussion" links.

--- resources/blog.js
+++ resources/blog.js
@@ -1,4 +1,12 @@
 head.ready(function() {
+
+  $('.discuss').each(function(_, element) {
+    $(element).delegate('.discuss-link', 'click', function() {
+      $(element).find('.discuss-content').show('slow');
+      $(element).find('.discuss-link').removeClass('discuss-link');
+    });
+  });
+
   /* Infinite Scroll */
   $('#main').infinitescroll({
     navSelector  : "#page-nav",            
